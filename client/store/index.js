22ef07701ef389580dac3a42b6be49d691aa6be0
Kristin Hill <krstinhill4211@gmail.com>
2022-03-01 14:03:27 -0800

    tea model created and seeded

--- client/store/index.js
+++ client/store/index.js
@@ -4,11 +4,11 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
 
-const reducer = combineReducers({ auth })
+const reducer = combineReducers({ auth });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
-const store = createStore(reducer, middleware)
+const store = createStore(reducer, middleware);
 
-export default store
-export * from './auth'
+export default store;
+export * from './auth';
