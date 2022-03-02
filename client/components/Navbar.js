034c85dfd8cd2f50c5e0ef49215e9832c5f2d4b3
Jessica Donig <jessdonig@gmail.com>
2022-03-02 13:50:05 -0800

    a few css ideas

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -5,25 +5,31 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>FS-App-Template</h1>
+    <h1>Hippo Tea</h1>
+    <h4>Made to order bubble teas</h4>
+    <img src='https://st2.depositphotos.com/15146240/48329/v/380/depositphotos_483299434-stock-illustration-cute-hippopotamus-drinking-boba-milk.jpg?forcejpeg=true' 
+    width="100"
+    height="100"/>
+    
     <nav>
-      {isLoggedIn ? (
+      {/*Jess commented out these lines to get the logged in experience... {isLoggedIn ? ( */}
         <div>
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
+          <Link to="/tea">Bubble Teas</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
         </div>
-      ) : (
+      {/* ) : ( */}
         <div>
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
         </div>
-      )}
+      {/* )} */}
     </nav>
-    <hr />
+    {/* <hr /> */}
   </div>
 )
 
