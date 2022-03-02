435fe81c3be3106772fb20909de51674dc26110b
Jessica Donig <jessdonig@gmail.com>
2022-03-02 12:39:11 -0800

    single tea route working

--- server/api/tea.js
+++ server/api/tea.js
@@ -10,3 +10,19 @@ router.get('/', async (req, res, next) => {
     next(err);
   }
 })
+
+// GET /api/teas/:id
+router.get('/:id', async (req, res, next) => {
+  try {
+    const tea = await Tea.findByPk(req.params.id);
+    if(!tea) {
+      let err = new Error('Umm...we can\'t find what you\'re looking for.');
+      err.status = 404;
+      next(err);
+    } else {
+      res.send(tea);
+    }
+  } catch (err) {
+    next(err);
+  }
+})
