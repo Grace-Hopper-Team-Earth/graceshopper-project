097e45e3d6db343a81d6af0678b41db1e231a3c0
Katelyn Miller <katdianemiller@gmail.com>
2022-03-02 16:59:35 -0500

    added allusers to redux store

--- client/store/users.js
+++ client/store/users.js
@@ -1,13 +1,21 @@
 import axios from 'axios';
 
 // ACTION TYPES
-const GET_SINGLE_USER = 'GET_SINGLE_USER'
+const GET_SINGLE_USER = 'GET_SINGLE_USER';
+const GET_ALL_USERS = 'GET_ALL_USERS';
 
 // ACTION CREATORS
 export const setSingleUser = (user) => {
     return {
         type: GET_SINGLE_USER,
         user
+    }
+}
+
+export const setAllUsers = (users) => {
+    return {
+        type: GET_ALL_USERS,
+        users
     }
 }
 
@@ -23,9 +31,21 @@ export const fetchSingleUser = (id) => {
     }
 }
 
+export const fetchAllUsers = () => {
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.get('/api/users')
+            dispatch(setAllUsers(data))
+        } catch (err) {
+            console.error(err)
+        }
+    }
+}
+
 // INITIAL STATE
 const initialState = {
-    singleUser: {}
+    singleUser: {},
+    allUsers: []
 }
 
 // SUB-REDUCER
@@ -33,5 +53,7 @@ export default function usersReducer(state = initialState, action) {
     switch (action.type) {
         case GET_SINGLE_USER:
             return {...state, singleUser: action.user}
+        case GET_ALL_USERS:
+            return {...state, allUsers: action.users}
     }
 }
