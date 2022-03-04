66f69066d0a002ab0d281cb47be5d1afaf99b3f7
Qina Zeng <qxqinazeng@gmail.com>
2022-03-04 18:08:43 -0500

    delete link for cart

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -43,7 +43,6 @@ const Navbar = ({ handleClick, isLoggedIn, user }) => {
             <Link to='/teas'>Bubble Teas</Link>
             <Link to='/login'>Login</Link>
             <Link to='/signup'>Sign Up</Link>
-            <Link to='/carts'>Cart</Link>
           </div>
         )}
       </nav>
