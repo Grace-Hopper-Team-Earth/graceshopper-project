94b9f25065d6ed20abb41c573309cedb0c3623b2
Jessica Donig <jessdonig@gmail.com>
2022-03-08 14:38:34 -0800

    add hippo gif

--- client/components/CheckoutDone.js
+++ client/components/CheckoutDone.js
@@ -3,9 +3,14 @@ import React from 'react';
 class CheckoutDone extends React.Component {
   render() {
     return (
-      <div>
+      <div >
         <h2>Thank you for ordering</h2>
         <h3>Enjoy your hippo tea!</h3>
+        <div display = 'flex'>
+        <img src= 'https://media.giphy.com/media/lqMg6hf8Mie9cvsrmi/giphy.gif'
+        align-items='center'/>
+        </div>
+        
       </div>
     );
   }
