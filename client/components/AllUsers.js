312d2205eb3d205f7a0bafc6cc614ef0cce9d356
Qina Zeng <qxqinazeng@gmail.com>
2022-03-08 15:48:16 -0500

    make a table for all user

--- client/components/AllUsers.js
+++ client/components/AllUsers.js
@@ -4,42 +4,70 @@ import { Link } from 'react-router-dom';
 import { fetchAllUsers } from '../store/users';
 
 class AllUsers extends React.Component {
-    componentDidMount() {
-        this.props.getAllUsers();
-    }
-    
-    render() {
-        const users = this.props.allUsers;
-        
-        return (
-            <div>
-                <h2>Registered Users:</h2>
-                <div>
-                    {users.map((user) => (
-                        <div key={user.id}>
-                            <Link to={`/users/${user.id}`}>
-                                <h5>{user.firstName + " " + user.lastName}</h5>
-                                <h5>{user.username}</h5>
-                                <h5>{user.isAdmin ? 'Admin' : ''}</h5>
-                            </Link>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        )
-    }
+  componentDidMount() {
+    this.props.getAllUsers();
+  }
+
+  render() {
+    const users = this.props.allUsers;
+
+    return (
+      <div>
+        <h2>Registered Users:</h2>
+        <div>
+          <table width='100%' border='1px solid'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Auth</th>
+                <th>More Details</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.id}>
+                  <td align='center'>{user.firstName + ' ' + user.lastName}</td>
+                  <td align='center'>{user.username}</td>
+                  <td align='center'>{user.isAdmin ? 'Admin' : ''}</td>
+                  <td align='center'>
+                    {<Link to={`/users/${user.id}`}>See Detalis</Link>}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+{
+  /* {users.map((user) => (
+              <div key={user.id}>
+                  <Link to={`/users/${user.id}`}>
+                      <tr>
+                      <th>{user.firstName + " " + user.lastName}</th>
+                      <th>{user.username}</th>
+                      <th>{user.isAdmin ? 'Admin' : ''}</th>
+                      </tr>
+                  </Link>
+              </div>
+          ))} */
 }
 
 const mapStateToProps = ({ users }) => {
-    return {
-        allUsers: users.allUsers
-    }
-}
+  return {
+    allUsers: users.allUsers,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        getAllUsers: () => dispatch(fetchAllUsers())
-    }
-}
+  return {
+    getAllUsers: () => dispatch(fetchAllUsers()),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(AllUsers)
+export default connect(mapStateToProps, mapDispatchToProps)(AllUsers);
